e4b9d999d2215eff874707f9e5ab8f5c2dccf0c5
Chandra Shekhar Shukla <75750252+Shekhar9521@users.noreply.github.com>
2021-05-12 00:36:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import pyttsx3
+import PyPDF2
+book = open('oop1.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+print(pages)
+speaker = pyttsx3.init()
+for num in range(10, pages):
+    page = pdfReader.getPage(10)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
